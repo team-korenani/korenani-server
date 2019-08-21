b78f99d2846a72903958f29b8ed4c26e9ac082fb
Go Nakano <g.nakano555@gmail.com>
2019-08-21 16:23:37 +0900

    succeeded analyzing photo & translate

--- controllers/index.js
+++ controllers/index.js
@@ -4,20 +4,38 @@ const axios = require("axios");
 
 require("dotenv").config({ path: `${__dirname}/../.env` });
 
-router.post("/photos", (req, res) => {
-  console.log(req);
-  // const results = await axios({
-  //   method: "post",
-  //   url:
-  //     "https://microsoft-azure-microsoft-computer-vision-v1.p.rapidapi.com/describe",
-  //   headers: {
-  //     "X-RapidAPI-Host":
-  //       "microsoft-azure-microsoft-computer-vision-v1.p.rapidapi.com",
-  //     "X-RapidAPI-Key": process.env.KEY
-  //   },
-  //   data: req
-  // });
-  // console.log("here the results!", results);
+router.post("/photos", async (req, res) => {
+  const imageData = req.files.steppico.data;
+  const responseFromMS = await axios({
+    method: "post",
+    url:
+      "https://microsoft-azure-microsoft-computer-vision-v1.p.rapidapi.com/describe",
+    headers: {
+      "X-RapidAPI-Host":
+        "microsoft-azure-microsoft-computer-vision-v1.p.rapidapi.com",
+      "X-RapidAPI-Key": process.env.KEY,
+      "Content-Type": "application/octet-stream"
+    },
+    data: imageData
+  });
+  const stringText = responseFromMS.data.description.tags.toString();
+
+  const responseFromMStext = await axios({
+    method: "get",
+    url: "https://microsoft-azure-translation-v1.p.rapidapi.com/translate",
+    headers: {
+      "X-RapidAPI-Host": "microsoft-azure-translation-v1.p.rapidapi.com",
+      "X-RapidAPI-Key": process.env.KEY,
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    params: {
+      from: "en",
+      to: "ja",
+      text: stringText
+    }
+  });
+  console.log("from MS vision", responseFromMS.data.description.tags);
+  console.log("here the translate", responseFromMStext.data);
   res.send("I have been naughty... ( ͡° ͜ʖ ͡°)");
 });
 
