6e80d1595d300e7fad8ae0ce3d9f7ee84c115a89
Steppico <steppashr@gmail.com>
2019-08-21 10:45:25 +0900

    create server and controllers folder, add routes in it

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("hello go!")
+const express = require("express");
+const app = express();
+const axios = require("axios");
+
+const port = process.env.PORT || 4000;
+
+const api = require("./controllers");
+
+app.use("/api", api);
+
+app.get("*", (req, res) => {
+  res.sendStatus(404);
+});
+app.listen(port, () => {
+  console.log(`Listening on sweet ol' port ${port}!`);
+});
