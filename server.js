54f88536a9a01363accebbbf9968bf2c649d0bc7
Wil Andrade and rina sakamaki <andradewilker+h0508g@gmail.com>
2019-08-22 18:25:30 +0900

    Add cors headers to allow x-origin requests and use middleware support to increase image size limit.

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
 const api = require("./controllers");
-const fileUpload = require("express-fileupload");
 const port = process.env.PORT || 4000;
 
-// app.use(fileUpload());
-app.use("/api", fileUpload(), express.json(), api);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(express.json({ limit: "50mb" }));
+
+app.use("/api", api);
 
 app.get("*", (req, res) => {
   res.status(404).send("Go is not here at the moment!");
